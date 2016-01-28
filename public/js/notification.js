505b775d8f74e1383f30c5db0b90e39dff4c6f26
jsmapr1 <joe.morgan@ku.edu>
2016-01-28 09:15:01 -0600

    Basic notification script #8 Includes - Requesting permission - Registering the service worker. - Resolving promise when worker is registered

---
+++ public/js/notification.js
@@ -0,0 +1,39 @@
+Notification.requestPermission();
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+  });
+  subscribe();
+} else {
+  console.log('Service workers aren\'t supported in this browser.');  
+}
+function subscribe() {
+    console.log('before subscribe');
+    navigator.serviceWorker.ready.then(function(reg) {
+      console.log('subscribe');
+      reg.pushManager.subscribe({userVisibleOnly: true})
+        .then(function(subscription) {
+          isPushEnabled = true;
+          subBtn.textContent = 'Unsubscribe from Push Messaging';
+          subBtn.disabled = false;
+          var endpoint = subscription.endpoint;
+          var key = subscription.getKey('p256dh');
+          updateStatus(endpoint,key,'subscribe');
+        })
+        .catch(function(e) {
+          if (Notification.permission === 'denied') {
+            console.log('Permission for Notifications was denied');
+          } else {
+            console.log('Unable to subscribe to push.', e);
+            subBtn.disabled = false;
+            subBtn.textContent = 'Subscribe to Push Messaging';
+          }
+        });
+    });
+}
